b877bd2dfe11a56f5462ea28fab10b12333e2e84
samuelfirst <a.a.sheykin@gamil.com>
2020-12-17 11:47:04 +0300

    Add Token model

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Token(models.Model):
+
+    access_token = models.CharField(max_length=40)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.access_token
