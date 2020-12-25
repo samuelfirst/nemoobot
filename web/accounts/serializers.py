cd07b62c48fdb89a9a425f667c85ff3559e7730c
samuelfirst <a.a.sheykin@gamil.com>
2020-12-25 17:52:54 +0300

    Edit serializers

--- web/accounts/serializers.py
+++ web/accounts/serializers.py
@@ -3,13 +3,20 @@ from rest_framework import serializers
 from .models import User, Token
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'twitch_username', 'twitch_user_id']
+class TokenSerializer(serializers.ModelSerializer):
 
-
-class TokenSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Token
-        fields = ['access_token', 'refresh_token', 'expires_in', 'user']
+        fields = [
+            'access_token', 'refresh_token', 'expires_in',
+        ]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    token = TokenSerializer()
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'twitch_username', 'twitch_user_id', 'token',
+        ]
