94f49c0cbaee502f9b85e41e56de5741b6ad4732
samuelfirst <a.a.sheykin@gamil.com>
2020-12-25 12:25:47 +0300

    Create TokenSerializer

--- web/accounts/serializers.py
+++ web/accounts/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import User
+from .models import User, Token
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'twitch_username', 'twitch_user_id']
+
+
+class TokenSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Token
+        fields = ['access_token', 'refresh_token', 'expires_in', 'user']
