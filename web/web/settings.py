40f8d330069e171a41d1d428f2b05b4173e877c6
samuelfirst <a.a.sheykin@gmail.com>
2020-12-30 13:24:16 +0400

    Change database settings

--- web/web/settings.py
+++ web/web/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('SQL_ENGINE'),
-        'NAME': BASE_DIR / os.getenv('SQL_DATABASE'),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
