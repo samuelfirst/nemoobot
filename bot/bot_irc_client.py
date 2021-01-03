627585a4e353ec6edc5ce9e2d60782a704dd6a89
samuelfirst <a.a.sheykin@gmail.com>
2021-01-04 00:21:07 +0400

    Create twitch bot with simple commands

---
+++ bot/bot_irc_client.py
@@ -0,0 +1,51 @@
+import os
+from typing import List
+from twisted.words.protocols import irc
+
+from twitch_bot import TwitchBot
+
+
+class BotIRCClient(irc.IRCClient):
+
+    bots: List[TwitchBot] = []
+
+    def __init__(self):
+        self.username = os.getenv('BOT_NICKNAME')
+        self.client_id = os.getenv('CLIENT_ID')
+        self.password = os.getenv('BOT_AUTH_TOKEN')
+
+    def signedOn(self):
+        self.factory.wait_time = 1
+
+        # self.sendLine("CAP REQ :twitch.tv/membership")
+        # self.sendLine("CAP REQ :twitch.tv/commands")
+        # self.sendLine("CAP REQ :twitch.tv/tags")
+
+        for bot in self.bots:
+            bot.irc = self
+            self.join(bot.channel)
+            self.joined(bot)
+
+    def lineReceived(self, line):
+        self.parse_line(line.decode('utf-8'))
+        super().lineReceived(line)
+
+    def parse_line(self, line):
+        pass
+
+    def privmsg(self, user, channel, message):
+        user = user.rsplit('!', 1)[0]
+        print(f"[{channel}] {user}: {message}")
+        for bot in self.bots:
+            if bot.channel == channel:
+                bot.antispam_check(user, message)
+                bot.process_command(user, message)
+
+    def joined(self, bot: TwitchBot):
+        print(f"{self.username} connected to {bot.channel}")
+        bot.write("Hello, there! I'm your TwitchBot")
+
+    def userJoined(self, user, channel):
+        for bot in self.bots:
+            if f'#{bot.channel}' == channel:
+                pass
---
+++ bot/commands/reload.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from .command import Command
+
+
+class Reload(Command):
+    API_BASE_URL = os.getenv('API_BASE_URL')
+
+    def load_user_settings_by_channel_id(self, channel_id):
+        url = self.API_BASE_URL + 'settings/'
+        res = requests.get(url)
+        
+        settings = res.json()
+        settings_list = list()
+        for user_settings in settings:
+            if user_settings['user']['twitch_user_id'] == channel_id:
+                user = user_settings['user']
+                default_commands = user_settings['default_commands']
+                custom_commands = user_settings['custom_commands']
+                antispam_settings = user_settings['antispam']
+                # add user settings to settings list
+                user_settings = (
+                    user, default_commands,
+                    custom_commands, antispam_settings
+                )
+
+        return user_settings
+
+    def match(self, bot, user, msg):
+        cmd = msg.split(' ')[0]
+        return cmd == '!reload'
+
+    def run(self, bot, user, msg):
+        user_settings = self.load_user_settings_by_channel_id(bot.channel_id)
+        bot.reload(*user_settings)
+        
