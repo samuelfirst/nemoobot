23002f0f35bff4d6b8a8e5a11a859eb94fac1377
samuelfirst <a.a.sheykin@gmail.com>
2021-01-13 13:59:04 +0300

    Create function to send 'ADD' command to bot

--- web/accounts/signals.py
+++ web/accounts/signals.py
@@ -20,20 +20,21 @@ def create_default_settings_for_new_user(sender, **kwargs):
 
 @receiver([post_save, post_delete], sender=CustomCommand)
 @receiver(post_save, sender=Setting)
-@receiver(post_save, sender=User)
 def send_reload_command_to_bot(sender, **kwargs):
-    if sender is User:
-        update_fields = kwargs.get('update_fields')
-        if update_fields and 'twitch_username' in update_fields:
-            user = kwargs.get('instance')
-            settings = Setting.objects.get(user_id=user.id)
-            send_command_to_bot.apply_async(('ADD', settings.id))
+    if sender is Setting:
+        settings = kwargs.get('instance')
+        if kwargs.get('created'):
+            return
     else:
-        if sender is Setting:
-            settings = kwargs.get('instance')
-            if kwargs.get('created'):
-                return
-        else:
-            custom_command = kwargs.get('instance')
-            settings = custom_command.settings
-        send_command_to_bot.apply_async(('RELOAD', settings.id))
+        custom_command = kwargs.get('instance')
+        settings = custom_command.settings
+    send_command_to_bot.apply_async(('RELOAD', settings.id))
+
+
+@receiver(post_save, sender=User)
+def send_add_command_to_bot(sender, **kwargs):
+    update_fields = kwargs.get('update_fields')
+    if update_fields and 'twitch_username' in update_fields:
+        user = kwargs.get('instance')
+        settings = Setting.objects.get(user_id=user.id)
+        send_command_to_bot.apply_async(('ADD', settings.id))
