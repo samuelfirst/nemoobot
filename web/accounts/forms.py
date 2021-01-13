d185a5ab5ca7021344d0fdbbcfba4824925788b0
samuelfirst <a.a.sheykin@gmail.com>
2021-01-13 13:26:39 +0300

    Delete cmd_name, cmd_reply, new_cmd_name, new_cmd_reply fields

--- web/accounts/forms.py
+++ web/accounts/forms.py
@@ -25,36 +25,19 @@ class SettingsChangeForm(forms.Form):
 
     default_commands = forms.JSONField(required=False)
     antispam_settings = forms.JSONField(required=False)
-    cmd_name = forms.JSONField()
-    cmd_reply = forms.JSONField()
-    new_cmd_name = forms.CharField(max_length=50, required=False)
-    new_cmd_reply = forms.CharField(max_length=200, required=False)
 
     def clean(self):
-        # TODO refactor clean method
         cleaned_data = super().clean()
 
         default_commands = cleaned_data.get('default_commands')
         antispam_settings = cleaned_data.get('antispam_settings')
-        cmd_names = cleaned_data.get('cmd_name')
-        cmd_replies = cleaned_data.get('cmd_reply')
-        if cmd_names is not None and cmd_replies is not None:
-            custom_commands = [
-                {'name': name, 'reply': reply} for name, reply in zip(cmd_names, cmd_replies)
-            ]
-        else:
-            custom_commands = []
-        new_custom_command = {
-            'name': cleaned_data.get('new_cmd_name'),
-            'reply': cleaned_data.get('new_cmd_reply'),
-        }
+
         if default_commands is None:
             default_commands = list()
         if antispam_settings is None:
             antispam_settings = list()
+
         self.cleaned_data = {
             'default_commands': default_commands,
             'antispam_settings': antispam_settings,
-            'custom_commands': custom_commands,
-            'new_custom_command': new_custom_command,
         }
