cb657da16bcda116495929bfeb50e2e42eec0b86
samuelfirst <a.a.sheykin@gmail.com>
2021-01-13 13:41:10 +0300

    Add DEFAULT_AUTHENTICATION_CLASSES to drf settings

--- web/web/settings.py
+++ web/web/settings.py
@@ -49,6 +49,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
