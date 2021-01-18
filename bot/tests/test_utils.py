39c68a0fed2888b7e62a5914e648ce5d1bef0fd4
samuelfirst <a.a.sheykin@gmail.com>
2021-01-19 00:40:43 +0300

    Edit utils tests

--- bot/tests/test_utils.py
+++ bot/tests/test_utils.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock
 from mock import patch
 from pytest_mock import mocker
 
-from bot.utils import load_user_settings, load_user_settings_by_channel_id
+from bot.utils import load_user_settings
 
 
 @pytest.fixture
@@ -15,7 +15,7 @@ def setup_mock_get_method(mocker):
 
 def test_load_user_settings_returns_list_if_any_error(setup_mock_get_method):
     mock_requests = setup_mock_get_method.return_value
-    mock_requests.json.return_value = 'not valid json'
+    mock_requests.json.return_value.raise_exception.side_effect = KeyError()
 
     result = load_user_settings()
     assert list() == result
