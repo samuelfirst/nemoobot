c6cfc2a59bc313e6cdeb36d1e0924a8bdd6a3035
samuelfirst <a.a.sheykin@gmail.com>
2021-01-21 19:50:16 +0300

    Move urls from web/urls.py

---
+++ web/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path(
+        'connect_to_twitch/',
+        views.connect_to_twicth,
+        name='connect_to_twitch'
+    ),
+    path('profile/', views.profile, name='profile'),
+    path('settings/', views.settings, name='settings'),
+]
