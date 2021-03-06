9ab351d88ba61fb2a0361b62e7118f0f3247b24b
samuelfirst <a.a.sheykin@gmail.com>
2021-03-06 21:17:31 +0300

    Remove signup view

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -17,21 +17,6 @@ def index(request):
     return render(request, 'index.html')
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('index')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'signup.html', {'form': form})
-
-
 def connect_to_twicth(request):
     code = request.GET.get('code')
     access_token, refresh_token, expires_in, expires_time = get_token_by_code(code)
