7c1a5c3db6d132030655c3414f979a56b0a20a2b
samuelfirst <a.a.sheykin@gmail.com>
2021-03-07 22:32:14 +0300

    Add vue-router

---
+++ frontend/src/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <Login></Login>
+    </div>
+</template>
+
+<script>
+import Login from './components/Login.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  },
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  width: 100vw;
+  min-width: 300px;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+</style>
