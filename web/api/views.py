0233197ec007fc3a1a123f7e6877676dfeb6fe2f
samuelfirst <a.a.sheykin@gmail.com>
2021-03-07 22:41:05 +0300

    Change views permissions classes to IsAuthenticated

--- web/api/views.py
+++ web/api/views.py
@@ -10,29 +10,31 @@ from accounts.models import Token, User, Setting, CustomCommand, Notice
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class TokenViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Token.objects.all()
     serializer_class = TokenSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class SettingViewSet(viewsets.ModelViewSet):
     queryset = Setting.objects.all()
     serializer_class = SettingSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CustomCommandsViewSet(viewsets.ModelViewSet):
     queryset = CustomCommand.objects.all()
     serializer_class = CustomCommandSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class NoticeViewSet(viewsets.ModelViewSet):
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class UserCreateView(generics.CreateAPIView):
