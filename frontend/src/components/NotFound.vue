dd1ee314d4e9388a96223c28b38bf49675b3fab8
samuelfirst <a.a.sheykin@gmail.com>
2021-03-07 22:33:59 +0300

    Create NotFound component

---
+++ frontend/src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mt-3 text-center">
+    <h1>Oops, it looks like the page you're looking for doesn't exist.</h1>
+    <router-link to="/" class="text-light">Back home.</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
